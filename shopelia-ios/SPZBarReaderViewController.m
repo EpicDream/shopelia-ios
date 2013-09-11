58f20270c6451e2fbc9e1d21bd612d32db2b1ed0
Amine Bellakrid <a.bellakrid@gmail.com>
2013-09-11 15:51:04 +0200

    Adding call to shopelia for product

--- shopelia-ios/SPZBarReaderViewController.m
+++ shopelia-ios/SPZBarReaderViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "SPZBarReaderViewController.h"
+#import "SPHTTPRequest.h"
 #import "overlayView.h"
 #import "UIView+Shopelia.h"
+
 
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO( v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 
@@ -63,6 +65,35 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+#pragma Zbar Delegate Implementation
+
+- (void) imagePickerController: (UIImagePickerController*) reader
+ didFinishPickingMediaWithInfo: (NSDictionary*) info
+{
+    // ADD: get the decode results
+    id<NSFastEnumeration> results =
+    [info objectForKey: ZBarReaderControllerResults];
+    ZBarSymbol *symbol = nil;
+    for(symbol in results)
+        // EXAMPLE: just grab the first barcode
+        break;
+    
+    SPHTTPRequest *request = [[SPHTTPRequest alloc] init];
+    NSString *url =@"http://api.prixing.fr/api/v4/products/E";
+    url = [url stringByAppendingFormat:@"%@",symbol.data];
+    url = [url stringByAppendingString:@"/?device=Samsung&lat=0&lng=0"];
+    [request setURL:[NSURL URLWithString:url]];
+    [request setHTTPMethod:@"GET"];
+    [request startWithCompletion:^(NSError *error, SPHTTPResponse *response){
+        if (error == nil) {
+            NSLog(@"%@",response.responseJSON);
+        } else {
+            NSLog(@"%@",error);
+        }
+    }];
+    // ADD: dismiss the controller (NB dismiss from the *reader*!)
+    [reader dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
