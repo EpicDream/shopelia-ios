1a2e9c0f6fe15f6b80d53f136d86803db4d0df97
Nicolas Bigot <nicolasbigot@shopelia.com>
2013-11-07 15:29:30 +0100

    Add (SPViewController+Utilities).

---
+++ shopelia-ios/SPViewController+Utilities.h
@@ -0,0 +1,15 @@
+//
+//  SPViewController+Utilities.h
+//  shopelia-ios
+//
+//  Created by Nicolas on 07/11/2013.
+//  Copyright (c) 2013 Shopelia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPViewController (Utilities)
+
++ (UIView *)firstEditingView:(UIView *)view;
+
+@end
